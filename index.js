db9ea2da29db5375b50026dc922a519496f2a590
Nicolas Marcora <nicolas.marcora@yahoo.co.uk>
2018-07-28 19:09:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ const logShout = string => console.log(shout(string))
 
 const logWhisper = string => console.log(whisper(string))
 
+const sayHiToGrandma = string => {
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+  
+  if (string === )
+}
+
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
